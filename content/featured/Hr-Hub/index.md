e1a760a4c34e403b7648c21a5383fe09c6afa4b5
Klodovsky <khaled.benhassen@polytechnicien.tn>
2021-05-30 18:08:33 +0200

    hr-hub project

---
+++ content/featured/Hr-Hub/index.md
@@ -0,0 +1,15 @@
+---
+date: '1'
+title: 'HR-Hub'
+cover: 'Hr-Hub.png'
+github: 'https://github.com/Klodovsky/HR-Hub'
+external: 'https://hr-hub-demo.netlify.app'
+tech:
+  - Vue.js
+  - JQuery
+  - CSS
+  - Vuex
+showInProjects: true
+---
+
+HR-Hub Template offers you the best template created by the latest version of vuejs 2.6.11 with super responsive web design.Mix supports several common CSS and JQuery. We have also added few reusable components which speedup your development..
--- public/render-page.js
+++ public/render-page.js
@@ -1,12 +1,12 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/@reach/router/index.js"), require("fs"), require("/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		module.exports = factory(require("/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/@reach/router/index.js"), require("fs"), require("/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else if(typeof define === 'function' && define.amd)
-		define("lib", ["/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/@reach/router/index.js", "fs", "/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
+		define("lib", ["/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/@reach/router/index.js", "fs", "/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
 	else if(typeof exports === 'object')
-		exports["lib"] = factory(require("/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/@reach/router/index.js"), require("fs"), require("/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		exports["lib"] = factory(require("/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/@reach/router/index.js"), require("fs"), require("/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else
-		root["lib"] = factory(root["/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/@reach/router/index.js"], root["fs"], root["/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
+		root["lib"] = factory(root["/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/@reach/router/index.js"], root["fs"], root["/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE__reach_router__, __WEBPACK_EXTERNAL_MODULE_fs__, __WEBPACK_EXTERNAL_MODULE_lodash_merge__, __WEBPACK_EXTERNAL_MODULE_path__, __WEBPACK_EXTERNAL_MODULE_react__, __WEBPACK_EXTERNAL_MODULE_react_dom_server__, __WEBPACK_EXTERNAL_MODULE_react_helmet__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -35783,9 +35783,9 @@ if ( true && typeof window !== 'undefined' && typeof navigator !== 'undefined' &
 /***/ }),
 
 /***/ "@reach/router":
-/*!************************************************************************************************!*\
-  !*** external "/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/@reach/router/index.js" ***!
-  \************************************************************************************************/
+/*!*******************************************************************************************************!*\
+  !*** external "/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/@reach/router/index.js" ***!
+  \*******************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -35805,9 +35805,9 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_fs__;
 /***/ }),
 
 /***/ "lodash/merge":
-/*!*****************************************************************************************!*\
-  !*** external "/home/khaled/Desktop/Khaled.benhassen.dev/node_modules/lodash/merge.js" ***!
-  \*****************************************************************************************/
+/*!************************************************************************************************!*\
+  !*** external "/home/khaled/Desktop/Github/Khaled.benhassen.dev/node_modules/lodash/merge.js" ***!
+  \************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
